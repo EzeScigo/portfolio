f9cdb2afa0fe84285d1e3b1427751a4a3de002c9
EzeScigo <scigolini@gmail.com>
2021-02-11 16:55:00 +0100

    more

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -7,9 +7,6 @@ const Footer = () => {
   return (
     <div className='footer-container'>
       <p className='footer-left'>Design & Developed by Ezequiel Scigolini</p>
-      <div className='footer-center'>
-        
-      </div>
       <p className='footer-right'>Copyright © 2021</p>
     </div>
 )};
